c8fd52886ec5896ccdb5749def68a983042387ae
Allink <arclicious@vivaldi.net>
2023-03-20 08:09:38 +0000

    fix: limit area effect clouds
    {BlockEntityTag: {MinSpawnDelay: 1, SpawnData: {entity: {ReapplicationDelay: 1, Particle: "elder_guardian", Duration: 32767, id: "minecraft:area_effect_cloud", Radius: 960.0f}}, MaxNearbyEntities: 2048, MaxSpawnDelay: 1, SpawnCount: 64, id: "minecraft:mob_spawner", SpawnPotentials: [{weight: 1, data: {entity: {ReapplicationDelay: 1, Particle: "elder_guardian", Duration: 32767, id: "minecraft:area_effect_cloud", Radius: 960.0f}}}], SpawnRange: 256, Delay: 1}

---
+++ mc/src/main/java/me/allinkdev/deviousmod/mixin/patches/entity/AreaEffectCloudParticleLimiter.java
@@ -0,0 +1,33 @@
+package me.allinkdev.deviousmod.mixin.patches.entity;
+
+import net.minecraft.entity.AreaEffectCloudEntity;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AreaEffectCloudEntity.class)
+public class AreaEffectCloudParticleLimiter {
+    @Inject(method = "getParticleType", at = @At("RETURN"), cancellable = true)
+    public void onGetParticleType(final CallbackInfoReturnable<ParticleEffect> cir) {
+        cir.setReturnValue(ParticleTypes.ENTITY_EFFECT);
+    }
+
+    @Inject(method = "getRadius", at = @At("RETURN"), cancellable = true)
+    public void onGetRadius(final CallbackInfoReturnable<Float> cir) {
+        cir.setReturnValue(MathHelper.clamp(cir.getReturnValue(), 0.0F, 5F));
+    }
+
+    @Inject(method = "getRadiusOnUse", at = @At("RETURN"), cancellable = true)
+    public void onGetRadiusOnUse(final CallbackInfoReturnable<Float> cir) {
+        cir.setReturnValue(MathHelper.clamp(cir.getReturnValue(), 0.0F, 5.0F));
+    }
+
+    @Inject(method = "getRadiusOnUse", at = @At("RETURN"), cancellable = true)
+    public void onGetRadiusGrowth(final CallbackInfoReturnable<Float> cir) {
+        cir.setReturnValue(MathHelper.clamp(cir.getReturnValue(), 0.0F, 5.0F));
+    }
+}
