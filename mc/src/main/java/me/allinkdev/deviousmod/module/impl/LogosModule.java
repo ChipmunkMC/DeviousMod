a01108022af41b222565055c2c46e8c2115fb686
Allink <arclicious@vivaldi.net>
2023-03-24 13:43:32 +0000

    refactor: add game version & improve performance of LogosModule

--- mc/src/main/java/me/allinkdev/deviousmod/module/impl/LogosModule.java
+++ mc/src/main/java/me/allinkdev/deviousmod/module/impl/LogosModule.java
@@ -4,12 +4,15 @@ import com.google.common.util.concurrent.AtomicDouble;
 import me.allinkdev.deviousmod.event.transformer.impl.InGameUITransformer;
 import me.allinkdev.deviousmod.module.DModule;
 import me.allinkdev.deviousmod.module.ModuleManager;
+import net.minecraft.SharedConstants;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.ColorHelper;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 public final class LogosModule extends DModule implements InGameUITransformer {
+    private static final String GAME_VERSION = SharedConstants.getGameVersion().getName();
+    private static final String TEXT = "DeviousMod " + GAME_VERSION;
     private static final AtomicDouble at = new AtomicDouble(0);
     private static final int WHITE = ColorHelper.Argb.getArgb(255, 255, 255, 255);
     private static final int BLACK = ColorHelper.Argb.getArgb(255, 0, 0, 0);
@@ -35,6 +38,10 @@ public final class LogosModule extends DModule implements InGameUITransformer {
 
     @Override
     public void render(final MatrixStack matrices, final float tickDelta, final CallbackInfo ci) {
+        if (client.options.debugEnabled) {
+            return;
+        }
+
         client.inGameHud.setCanShowChatDisabledScreen(true);
 
         final double w = at.addAndGet(.05);
@@ -44,7 +51,8 @@ public final class LogosModule extends DModule implements InGameUITransformer {
         final TextRenderer textRenderer = client.textRenderer;
         final MatrixStack shadowStack = new MatrixStack();
         shadowStack.translate(x, y, 0);
-        textRenderer.draw(shadowStack, "DeviousMod", 4, 4, BLACK);
-        textRenderer.draw(matrices, "DeviousMod", 4, 4, WHITE);
+
+        textRenderer.draw(shadowStack, TEXT, 4, 4, BLACK);
+        textRenderer.draw(matrices, TEXT, 4, 4, WHITE);
     }
 }
