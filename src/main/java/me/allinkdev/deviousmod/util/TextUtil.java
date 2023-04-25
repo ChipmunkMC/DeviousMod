c72ef1227093b2543340abe83176649aa0f61175
Allink <arclicious@vivaldi.net>
2023-04-25 16:14:15 +0100

    refactor: change static final variable to upper snake case

--- src/main/java/me/allinkdev/deviousmod/util/TextUtil.java
+++ src/main/java/me/allinkdev/deviousmod/util/TextUtil.java
@@ -8,9 +8,9 @@ import net.minecraft.text.Text;
 
 public final class TextUtil extends NoConstructor {
     public static final MutableText INVALID_JSON = MutableText.of(new LiteralTextContent("Invalid JSON!"));
-    private static final FabricClientAudiences audience = FabricClientAudiences.of();
+    private static final FabricClientAudiences AUDIENCE = FabricClientAudiences.of();
 
     public static Text toText(final Component component) {
-        return audience.toNative(component);
+        return AUDIENCE.toNative(component);
     }
 }
