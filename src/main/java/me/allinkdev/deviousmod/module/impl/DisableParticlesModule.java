80163c40346752ce658d815c0026b62f0748584b
Allink <arclicious@vivaldi.net>
2023-04-30 19:01:36 +0100

    feat: cancel splash potions in DisableParticlesModule

--- src/main/java/me/allinkdev/deviousmod/module/impl/DisableParticlesModule.java
+++ src/main/java/me/allinkdev/deviousmod/module/impl/DisableParticlesModule.java
@@ -7,6 +7,8 @@ import me.allinkdev.deviousmod.module.ModuleManager;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.ExperienceOrbSpawnS2CPacket;
 import net.minecraft.network.packet.s2c.play.ParticleS2CPacket;
+import net.minecraft.network.packet.s2c.play.WorldEventS2CPacket;
+import net.minecraft.world.WorldEvents;
 
 public final class DisableParticlesModule extends DModule {
     public DisableParticlesModule(final ModuleManager moduleManager) {
@@ -34,6 +36,15 @@ public final class DisableParticlesModule extends DModule {
 
         if (packet instanceof ParticleS2CPacket || packet instanceof ExperienceOrbSpawnS2CPacket) {
             event.setCancelled(true);
+            return;
+        }
+
+        if (packet instanceof final WorldEventS2CPacket worldEventPacket) {
+            final int eventId = worldEventPacket.getEventId();
+
+            if (eventId == WorldEvents.SPLASH_POTION_SPLASHED || eventId == WorldEvents.INSTANT_SPLASH_POTION_SPLASHED) {
+                event.setCancelled(true);
+            }
         }
     }
 }
