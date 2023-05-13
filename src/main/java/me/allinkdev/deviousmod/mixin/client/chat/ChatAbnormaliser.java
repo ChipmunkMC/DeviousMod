6ba9d8db97e658d139867232809b05d84be27fb4
Allink <arclicious@vivaldi.net>
2023-05-13 21:51:44 +0100

    feat: do not normalise chat strings

---
+++ src/main/java/me/allinkdev/deviousmod/mixin/client/chat/ChatAbnormaliser.java
@@ -0,0 +1,14 @@
+package me.allinkdev.deviousmod.mixin.client.chat;
+
+import net.minecraft.client.gui.screen.ChatScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ChatScreen.class)
+public final class ChatAbnormaliser {
+    @Redirect(method = "sendMessage", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ChatScreen;normalize(Ljava/lang/String;)Ljava/lang/String;"))
+    private String onNormalize(final ChatScreen instance, final String chatText) {
+        return chatText;
+    }
+}
