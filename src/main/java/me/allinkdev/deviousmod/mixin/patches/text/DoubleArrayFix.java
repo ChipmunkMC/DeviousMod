7219bbb46f4979ba9f1cacb3ab7e37a7d1e0c6da
Luna <90072930+LunaWasFlaggedAgain@users.noreply.github.com>
2023-05-29 19:12:22 -0300

    Add fix for empty arrays in extra array (#14)

---
+++ src/main/java/me/allinkdev/deviousmod/mixin/patches/text/DoubleArrayFix.java
@@ -0,0 +1,23 @@
+package me.allinkdev.deviousmod.mixin.patches.text;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.lang.reflect.Type;
+
+@Mixin(Text.Serializer.class)
+public final class DoubleArrayFix {
+    @Inject(method = "deserialize(Lcom/google/gson/JsonElement;Ljava/lang/reflect/Type;Lcom/google/gson/JsonDeserializationContext;)Lnet/minecraft/text/MutableText;", at = @At("HEAD"))
+    private void deserialize(final JsonElement jsonElement, final Type type, final JsonDeserializationContext jsonDeserializationContext, final CallbackInfoReturnable<MutableText> cir) {
+        if (jsonElement.isJsonArray() && jsonElement.getAsJsonArray().size() <= 0) {
+            throw new JsonParseException("Unexpected empty array of components");
+        }
+    }
+}
