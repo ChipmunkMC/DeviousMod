ff9044346f99ebd7df7bed98a7a6caac0460c145
Allink <arclicious@vivaldi.net>
2023-06-12 00:19:17 +0100

    refactor: remove datacompound class

--- src/main/java/me/allinkdev/deviousmod/data/exception/DataDirCreateException.java
+++
@@ -1,9 +0,0 @@
-package me.allinkdev.deviousmod.data.exception;
-
-import java.io.File;
-
-public final class DataDirCreateException extends RuntimeException {
-    public DataDirCreateException(final File file) {
-        super(String.format("Failed to create directory %s!", file));
-    }
-}
