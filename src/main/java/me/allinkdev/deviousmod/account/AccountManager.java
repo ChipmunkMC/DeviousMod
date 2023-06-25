9bc87b3e13713bf48b2b1c0f48eb6e5c292bea04
Allink <arclicious@vivaldi.net>
2023-06-25 09:30:43 +0100

    feat: allow changing of username in the multiplayer screen

--- src/main/java/me/allinkdev/deviousmod/account/AccountManager.java
+++ src/main/java/me/allinkdev/deviousmod/account/AccountManager.java
@@ -1,8 +1,15 @@
 package me.allinkdev.deviousmod.account;
 
+import com.google.common.eventbus.Subscribe;
 import me.allinkdev.deviousmod.DeviousMod;
+import me.allinkdev.deviousmod.event.screen.impl.InitScreenEvent;
+import me.allinkdev.deviousmod.mixin.accessor.ScreenAccessor;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.util.Session;
+import net.minecraft.text.Text;
 import net.minecraft.util.Uuids;
 
 import java.util.Optional;
@@ -28,6 +35,25 @@ public class AccountManager {
         );
     }
 
+    private void operateOnScreen(final Screen screen, final ScreenAccessor accessor) {
+        final int width = 200;
+        final int height = 10;
+        final TextFieldWidget textFieldWidget = new TextFieldWidget(this.client.textRenderer, screen.width - (width + 4), screen.height - (height + 4), width, height, Text.empty());
+        textFieldWidget.setMaxLength(16);
+        textFieldWidget.setText(this.getCurrentUsername());
+        textFieldWidget.setChangedListener(username -> setUsernameWidget(textFieldWidget, username));
+        accessor.invokeAddDrawableChild(textFieldWidget);
+    }
+
+    @Subscribe
+    public void onScreenInit(final InitScreenEvent event) {
+        if (!(event.getScreen() instanceof MultiplayerScreen)) {
+            return;
+        }
+
+        event.addConsumer(this::operateOnScreen);
+    }
+
     public void setSession(final Session session) {
         this.client.session = session;
     }
@@ -36,8 +62,26 @@ public class AccountManager {
         return this.originalSession.getUsername();
     }
 
+    private void setUsernameWidget(final TextFieldWidget textFieldWidget, final String newUsername) {
+        if (newUsername.isEmpty() || newUsername.length() > 16) {
+            textFieldWidget.setText(this.getCurrentUsername());
+            return;
+        }
+
+        this.setUsername(newUsername);
+    }
+
     public void setUsername(final String username) {
+        if (username.equals(this.getOriginalUsername())) {
+            this.restoreSession();
+            return;
+        }
+
         this.client.session = new Session(username, Uuids.getOfflinePlayerUuid(username).toString(), "", Optional.empty(), Optional.empty(), Session.AccountType.LEGACY);
+    }
+
+    public String getCurrentUsername() {
+        return this.client.session.getUsername();
     }
 
     public void restoreSession() {
