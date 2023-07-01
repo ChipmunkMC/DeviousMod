c64cc1376889cc44c15eb93c05c3e48fc8e51c2c
Allink <arclicious@vivaldi.net>
2023-07-01 15:53:42 +0100

    fix: use old semantics for movement enforcer

--- src/main/java/me/allinkdev/deviousmod/module/impl/MovementEnforcerModule.java
+++ src/main/java/me/allinkdev/deviousmod/module/impl/MovementEnforcerModule.java
@@ -7,7 +7,6 @@ import me.allinkdev.deviousmod.event.network.packet.impl.PacketS2CEvent;
 import me.allinkdev.deviousmod.event.time.second.ServerSecondEvent;
 import me.allinkdev.deviousmod.module.CommandDependentModule;
 import me.allinkdev.deviousmod.module.DModuleManager;
-import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
 
 public final class MovementEnforcerModule extends CommandDependentModule {
@@ -82,43 +81,43 @@ public final class MovementEnforcerModule extends CommandDependentModule {
     private void onPacketReceive(final PacketS2CEvent event) {
         // TODO: Death stuff
 
-        if (!this.commandPresent) {
+        if (!this.commandPresent || this.client.player == null) {
             return;
         }
 
-        final Packet<?> packet = event.getPacket();
+        if (!(event.getPacket() instanceof final PlayerPositionLookS2CPacket positionPacket)) {
+            return;
+        }
+        
+        final double serverX = positionPacket.getX();
+        final double serverY = positionPacket.getY();
+        final double serverZ = positionPacket.getZ();
+        final double serverYaw = positionPacket.getYaw();
+        final double serverPitch = positionPacket.getPitch();
 
-        if (packet instanceof final PlayerPositionLookS2CPacket positionPacket) {
-            final double serverX = positionPacket.getX();
-            final double serverY = positionPacket.getY();
-            final double serverZ = positionPacket.getZ();
-            final double serverYaw = positionPacket.getYaw();
-            final double serverPitch = positionPacket.getPitch();
+        final double clientX = this.client.player.getX();
+        final double clientY = this.client.player.getY();
+        final double clientZ = this.client.player.getZ();
+        final float clientYaw = this.client.player.getYaw();
+        final float clientPitch = this.client.player.getPitch();
 
-            final double clientX = this.client.player.getX();
-            final double clientY = this.client.player.getY();
-            final double clientZ = this.client.player.getZ();
-            final float clientYaw = this.client.player.getYaw();
-            final float clientPitch = this.client.player.getPitch();
+        final double diffX = Math.abs(serverX - clientX);
+        final double diffY = Math.abs(serverY - clientY);
+        final double diffZ = Math.abs(serverZ - clientZ);
+        final float diffYaw = (float) Math.abs(serverYaw - clientYaw);
+        final float diffPitch = (float) Math.abs(serverPitch - clientPitch);
 
-            final double diffX = Math.abs(serverX - clientX);
-            final double diffY = Math.abs(serverY - clientY);
-            final double diffZ = Math.abs(serverZ - clientZ);
-            final float diffYaw = (float) Math.abs(serverYaw - clientYaw);
-            final float diffPitch = (float) Math.abs(serverPitch - clientPitch);
-
-            // TODO: Configurable threshold
-            if (diffX > 3 || diffY > 3 || diffZ > 3 || diffYaw > 3 || diffPitch > 3) {
-                this.targetX = clientX;
-                this.targetY = clientY;
-                this.targetZ = clientZ;
-                this.targetYaw = clientYaw;
-                this.targetPitch = clientPitch;
-                this.enforce = true;
-            } else if (diffX < 3 && diffY < 3 && diffZ < 3 && diffYaw < 3 && diffPitch < 3) {
-                this.enforce = false;
-                this.purge();
-            }
+        // TODO: Configurable threshold
+        if (diffX > 3 || diffY > 3 || diffZ > 3 || diffYaw > 3 || diffPitch > 3) {
+            this.targetX = clientX;
+            this.targetY = clientY;
+            this.targetZ = clientZ;
+            this.targetYaw = clientYaw;
+            this.targetPitch = clientPitch;
+            this.enforce = true;
+        } else if (diffX < 3 && diffY < 3 && diffZ < 3 && diffYaw < 3 && diffPitch < 3) {
+            this.enforce = false;
+            this.purge();
         }
     }
 }
