8e12619eb6b1ff40be458fd72cba0bddd4d3219f
Allink <arclicious@vivaldi.net>
2023-07-01 03:37:11 +0100

    fix: just cancel nbt tag size tracker add method

--- src/main/java/me/allinkdev/deviousmod/mixin/patches/tag/BigTagKickFix.java
+++ src/main/java/me/allinkdev/deviousmod/mixin/patches/tag/BigTagKickFix.java
@@ -1,24 +1,15 @@
 package me.allinkdev.deviousmod.mixin.patches.tag;
 
 import net.minecraft.nbt.NbtTagSizeTracker;
-import org.objectweb.asm.Opcodes;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(NbtTagSizeTracker.class)
 public final class BigTagKickFix {
-    @Shadow
-    private long allocatedBytes;
-
-    @Shadow
-    @Final
-    private long maxBytes;
-
-    @Redirect(method = "add", at = @At(value = "FIELD", target = "Lnet/minecraft/nbt/NbtTagSizeTracker;allocatedBytes:J", opcode = Opcodes.PUTFIELD))
-    private void onAdd(final NbtTagSizeTracker instance, final long value) {
-        this.allocatedBytes = Math.min(this.allocatedBytes + 1, this.maxBytes);
+    @Inject(method = "add", at = @At("HEAD"), cancellable = true)
+    private void onAdd(final long bytes, final CallbackInfo ci) {
+        ci.cancel();
     }
 }
