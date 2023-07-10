8ea9117a426dfc6aa61194493868239929116944
Allink <arclicious@vivaldi.net>
2023-07-10 04:45:26 +0100

    fix: use @WrapOperation more
    fixes incompatibility with noxesium

--- src/main/java/me/allinkdev/deviousmod/mixin/client/render/world/SodiumWorldRenderListener.java
+++ src/main/java/me/allinkdev/deviousmod/mixin/client/render/world/SodiumWorldRenderListener.java
@@ -1,5 +1,7 @@
 package me.allinkdev.deviousmod.mixin.client.render.world;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import me.allinkdev.deviousmod.event.render.block.PreBlockEntityRenderEvent;
 import me.allinkdev.deviousmod.util.EventUtil;
 import net.minecraft.block.entity.BlockEntity;
@@ -8,17 +10,16 @@ import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(targets = "me/jellysquid/mods/sodium/client/render/SodiumWorldRenderer")
 public final class SodiumWorldRenderListener {
-    @Redirect(method = "renderTileEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/block/entity/BlockEntityRenderDispatcher;render(Lnet/minecraft/block/entity/BlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;)V"))
+    @WrapOperation(method = "renderTileEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/block/entity/BlockEntityRenderDispatcher;render(Lnet/minecraft/block/entity/BlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;)V"))
     private <E extends BlockEntity> void onGetVisibleBlockEntities(final BlockEntityRenderDispatcher instance, final E blockEntity, final float tickDelta,
-                                                                   final MatrixStack matrices, final VertexConsumerProvider vertexConsumers) {
+                                                                   final MatrixStack matrices, final VertexConsumerProvider vertexConsumers, final Operation<Void> operation) {
         if (EventUtil.postCancellable(new PreBlockEntityRenderEvent(blockEntity))) {
             return;
         }
 
-        instance.render(blockEntity, tickDelta, matrices, vertexConsumers);
+        operation.call(instance, blockEntity, tickDelta, matrices, vertexConsumers);
     }
 }
