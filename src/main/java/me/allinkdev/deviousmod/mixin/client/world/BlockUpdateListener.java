39a200346781906a2352fa8c8a4de51eae64fbac
Allink <arclicious@vivaldi.net>
2023-08-06 20:03:55 +0100

    fix: rewrite block update listener to prevent conflicts

--- src/main/java/me/allinkdev/deviousmod/mixin/client/world/BlockUpdateListener.java
+++ src/main/java/me/allinkdev/deviousmod/mixin/client/world/BlockUpdateListener.java
@@ -3,30 +3,22 @@ package me.allinkdev.deviousmod.mixin.client.world;
 import me.allinkdev.deviousmod.event.world.chunk.BlockStateUpdateEvent;
 import me.allinkdev.deviousmod.util.EventUtil;
 import net.minecraft.block.BlockState;
-import net.minecraft.client.world.ClientWorld;
-import net.minecraft.registry.DynamicRegistryManager;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.profiler.Profiler;
-import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.World;
-import net.minecraft.world.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.function.Supplier;
+@Mixin(World.class)
+public final class BlockUpdateListener {
+    @Shadow @Final public boolean isClient;
 
-@Mixin(ClientWorld.class)
-public abstract class BlockUpdateListener extends World {
-
-    protected BlockUpdateListener(final MutableWorldProperties properties, final RegistryKey<World> registryRef, final DynamicRegistryManager registryManager,
-                                  final RegistryEntry<DimensionType> dimensionEntry, final Supplier<Profiler> profiler, final boolean isClient,
-                                  final boolean debugWorld, final long biomeAccess, final int maxChainedNeighborUpdates) {
-        super(properties, registryRef, registryManager, dimensionEntry, profiler, isClient, debugWorld, biomeAccess, maxChainedNeighborUpdates);
-    }
-
-    @Override
-    public void onBlockChanged(final BlockPos pos, final BlockState oldBlock, final BlockState newBlock) {
+    @Inject(method = "onBlockChanged", at = @At("HEAD"))
+    public void onBlockChanged(final BlockPos pos, final BlockState oldBlock, final BlockState newBlock, final CallbackInfo ci) {
+        if (!this.isClient) return;
         EventUtil.postEvent(new BlockStateUpdateEvent(pos, oldBlock, newBlock));
     }
 }
